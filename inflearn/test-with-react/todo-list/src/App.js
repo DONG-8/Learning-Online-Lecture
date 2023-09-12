78b8e7a7ca544137143ed0eb736652202307cd4d
DONG-8 <junzzamg9@gmail.com>
2023-09-12 15:41:28 +0900

    🧪 test : 종합 test 완료 (#7) - tab, form, list 간 data 이동 및 컴포넌트 렌더링 테스트 진행

--- inflearn/test-with-react/todo-list/src/App.js
+++ inflearn/test-with-react/todo-list/src/App.js
@@ -8,11 +8,11 @@ import TodoList from "./components/TodoList";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [tabState, setTabState] = useState("todo");
-  const [data, setData] = useState([
-    { todo: "할일하기", check: false, favorit: false, idx: 0 },
-    { todo: "테스트 공부하기", check: true, favorit: true, idx: 1 },
-    { todo: "운동 1시간 하기", check: false, favorit: true, idx: 2 },
-  ]);
+  // const [data, setData] = useState([
+  //   { todo: "할일하기", check: false, favorit: false, idx: 0 },
+  //   { todo: "테스트 공부하기", check: true, favorit: true, idx: 1 },
+  //   { todo: "운동 1시간 하기", check: false, favorit: true, idx: 2 },
+  // ]);
 
   function handleTodo(todo) {
     const idxLength = todoList.length;
@@ -24,7 +24,7 @@ function App() {
   return (
     <TodoWrapper>
       <TodoTab {...props} />
-      <TodoList tabState={tabState} data={data} setData={setData} />
+      <TodoList tabState={tabState} data={todoList} setData={setTodoList} />
       <TodoForm setTodoList={handleTodo} />
     </TodoWrapper>
   );
--- inflearn/test-with-react/todo-list/src/App.test.js
+++ inflearn/test-with-react/todo-list/src/App.test.js
@@ -1,10 +1,63 @@
-import { render, screen } from "@testing-library/react";
-import TodoWrapper from "./components/TodoWrapper";
+import { logRoles, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-describe("todo List 컴포넌트 종합 테스트", () => {});
+// 각각의 렌더링 여부는 각 컴포넌트 test를 통해 확인 함.
+// 종합적인 동작 테스트를 진행
+describe("todo List 컴포넌트 종합 테스트", () => {
+  test("입력 값에 따른 list item rendering 동작 테스트", async () => {
+    render(<App />);
+    const todoFormInputElement = screen.getByPlaceholderText("작업 추가");
+    const todoFormButton = screen.getByRole("button", {
+      name: "+",
+    });
+    await userEvent.type(todoFormInputElement, "할일하기");
+    await userEvent.click(todoFormButton);
+    // 입력값에 따른 렌더링이 되었는가
+    const todoListElements = await screen.findAllByTestId("todo-item");
+    expect(todoListElements).toHaveLength(1);
+    const todoItem = screen.getByText("할일하기");
+    expect(todoItem).toBeInTheDocument();
+  });
 
-test("renders learn react link", () => {
-  render(<TodoWrapper />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  test("list Element의 check, favorit을 눌렸을 때, 화면상에 올바르게 렌더링 되는가", async () => {
+    // 임의 데이터 생성
+    render(<App />);
+    const todoFormInputElement = screen.getByPlaceholderText("작업 추가");
+    const todoFormButton = screen.getByRole("button", {
+      name: "+",
+    });
+    await userEvent.type(todoFormInputElement, "할일하기");
+    await userEvent.click(todoFormButton);
+    const checkBoxItem = screen.getByRole("checkbox");
+    const checkedItem = screen.getByText("할일하기");
+    await userEvent.click(checkBoxItem);
+    expect(checkBoxItem).toBeChecked();
+    expect(checkedItem).not.toBeInTheDocument();
+
+    const TabDoneElement = screen.getByTestId("todo-done");
+    await userEvent.click(TabDoneElement);
+    const doneListItem = screen.getByText("할일하기");
+    expect(doneListItem).toBeInTheDocument();
+
+    await userEvent.type(todoFormInputElement, "테스트 재밋당");
+    await userEvent.click(todoFormButton);
+
+    const todoTabElement = screen.getByTestId("todo-todo");
+    await userEvent.click(todoTabElement);
+
+    const todoItems = screen.getAllByTestId("todo-item");
+    const todoFavoritItem = screen.getByText("테스트 재밋당");
+    expect(todoItems).toHaveLength(1);
+    expect(todoFavoritItem).toBeInTheDocument();
+
+    const todoFavoritImg = screen.getByRole("img");
+    await userEvent.click(todoFavoritImg);
+
+    const TabFavoritElement = screen.getByTestId("todo-favorit");
+    await userEvent.click(TabFavoritElement);
+    const favoritLists = screen.getAllByTestId("todo-item");
+    expect(todoFavoritItem).toBeInTheDocument();
+    expect(favoritLists).toHaveLength(1);
+  });
 });
